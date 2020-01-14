d536495030f6c047e22f48ca32edcd0de3f42a59
Jeffrey Bosch <jefiozie.bosch@gmail.com>
2020-01-14 09:27:03 +0100

    test(builder): add initial test that just runs builder

--- builder/deploy/index.ts
+++ builder/deploy/index.ts
@@ -1,38 +1,38 @@
 import {
   BuilderContext,
   BuilderOutput,
-  createBuilder
-} from "@angular-devkit/architect";
-import { experimental, normalize } from "@angular-devkit/core";
-import { NodeJsSyncHost } from "@angular-devkit/core/node";
-import { Options } from "./Options";
-import * as glob from "glob";
-import { Uploader } from "./uploader";
+  createBuilder,
+} from '@angular-devkit/architect';
+import { experimental, normalize } from '@angular-devkit/core';
+import { NodeJsSyncHost } from '@angular-devkit/core/node';
+import { Options } from './Options';
+import * as glob from 'glob';
+import { Uploader } from './uploader';
 
 export default createBuilder<any>(
   async (builderConfig: Options, context: BuilderContext): Promise<any> => {
     const root = normalize(context.workspaceRoot);
     const workspace = new experimental.workspace.Workspace(
       root,
-      new NodeJsSyncHost()
+      new NodeJsSyncHost(),
     );
     await workspace
-      .loadWorkspaceFromHost(normalize("angular.json"))
+      .loadWorkspaceFromHost(normalize('angular.json'))
       .toPromise();
 
     if (!context.target) {
-      throw new Error("Cannot deploy the application without a target");
+      throw new Error('Cannot deploy the application without a target');
     }
 
     let buildResult: BuilderOutput;
     const configuration = builderConfig.mode
       ? builderConfig.mode
-      : "production";
+      : 'production';
 
     const build = await context.scheduleTarget({
-      target: "build",
-      project: context.target !== undefined ? context.target.project : "",
-      configuration
+      target: 'build',
+      project: context.target !== undefined ? context.target.project : '',
+      configuration,
     });
 
     buildResult = await build.result;
@@ -44,33 +44,33 @@ export default createBuilder<any>(
 
       if (files.length === 0) {
         throw new Error(
-          "Target did not produce any files, or the path is incorrect."
+          'Target did not produce any files, or the path is incorrect.',
         );
       }
       if (builderConfig.accessKeyId || builderConfig.secretAccessKey) {
-        context.logger.info("Start uploading files...");
+        context.logger.info('Start uploading files...');
         const uploader = new Uploader(context);
         await uploader.upload(files, filesPath, builderConfig);
-        context.logger.info("✔ Finished uploading files...");
+        context.logger.info('✔ Finished uploading files...');
         return { success: true };
       } else {
         return {
           error: `❌  Missing authentication settings for AWS`,
-          success: false
+          success: false,
         };
       }
     } else {
       return {
         error: `❌ Application build failed`,
-        success: false
+        success: false,
       };
     }
     function getFiles(filesPath: string) {
       return glob.sync(`**`, {
-        ignore: [".git"],
+        ignore: ['.git'],
         cwd: filesPath,
-        nodir: true
+        nodir: true,
       });
     }
-  }
+  },
 );
