b342eaba06d7125862e165cd3952e983ec99233c
Jefiozie <jefiozie.bosch@gmail.com>
2020-05-28 11:20:25 +0200

    feat: add multiple configurations set for deploy and fix no-build

--- builder/deploy/uploader.ts
+++ builder/deploy/uploader.ts
@@ -1,10 +1,10 @@
-import { BuilderContext } from "@angular-devkit/architect";
-import * as AWS from "aws-sdk";
-import { PutObjectRequest } from "aws-sdk/clients/s3";
+import { BuilderContext } from '@angular-devkit/architect';
+import * as AWS from 'aws-sdk';
+import { PutObjectRequest } from 'aws-sdk/clients/s3';
 import * as mimeTypes from 'mime-types';
-import * as fs from "fs";
-import * as path from "path";
-import { Schema } from "./schema";
+import * as fs from 'fs';
+import * as path from 'path';
+import { Schema } from './schema';
 import {
   getAccessKeyId,
   getBucket,
@@ -24,7 +24,7 @@ export class Uploader {
       const region = getRegion(builderConfig);
       if (!region || !bucket) {
         this._context.logger.error(
-          `❌  Looks like you are missing some configuration`
+          `❌  Looks like you are missing some configuration`,
         );
         return;
       }
@@ -32,12 +32,16 @@ export class Uploader {
       return;
     }
     return Promise.all(
-      files.map(async file => {
+      files.map(async (file) => {
         await this.uploadFile(builderConfig, path.join(filesPath, file), file);
-      })
+      }),
     );
   }
-  public async uploadFile(options: Schema, localFilePath: string, originFilePath: string) {
+  public async uploadFile(
+    options: Schema,
+    localFilePath: string,
+    originFilePath: string,
+  ) {
     AWS.config.update({ region: options.region });
 
     const s3 = new AWS.S3({
@@ -47,23 +51,27 @@ export class Uploader {
     });
     const fileName = path.basename(localFilePath);
     const body = fs.createReadStream(localFilePath);
-    body.on("error", function(err) {
-      console.log("File Error", err);
+    body.on('error', function (err) {
+      console.log('File Error', err);
     });
     const params: PutObjectRequest = {
       Bucket: getBucket(options) || '',
-      Key: options.subFolder ? `${options.subFolder}/${originFilePath}` : originFilePath,
+      Key: options.subFolder
+        ? `${options.subFolder}/${originFilePath}`
+        : originFilePath,
       Body: body,
       ContentType: mimeTypes.lookup(fileName) || undefined,
     };
     await s3
       .upload(params)
       .promise()
-      .then(e =>
-        this._context.logger.info(`Uploaded file "${e.Key}" to ${e.Location}`)
+      .then((file) =>
+        this._context.logger.info(
+          `Uploaded file "${file.Key}" to ${file.Location}`,
+        ),
       )
-      .catch(item =>
-        this._context.logger.error(`Error uploading file: ${item.Key}`)
+      .catch((item) =>
+        this._context.logger.error(`Error uploading file: ${item}`),
       );
   }
 }
