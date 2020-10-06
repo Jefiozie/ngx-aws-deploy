10ef5c11145e0bffc2bc49d94b77a15a0531bb29
Jefiozie <jefiozie.bosch@gmail.com>
2020-10-06 08:07:00 +0200

    chore: handle assets schema copy

--- libs/ngx-aws-deploy/src/lib/deploy/schema.d.ts
+++ libs/ngx-aws-deploy/src/lib/deploy/schema.d.ts
@@ -1,28 +1,10 @@
 export interface Schema {
-  configuration?: Configuration;
-  noBuild?: NoBuild;
-  baseHref?: AccessKeyID;
-  accessKeyId?: AccessKeyID;
-  secretAccessKey?: AccessKeyID;
-  region?: AccessKeyID;
-  bucket?: AccessKeyID;
-  subFolder?: AccessKeyID;
-}
-
-export interface AccessKeyID {
-  type?: string;
-  description?: string;
-}
-
-export interface Configuration {
-  type?: string;
-  default?: string;
-  description?: string;
-  alias?: string;
-}
-
-export interface NoBuild {
-  type?: string;
-  default?: boolean;
-  description?: string;
+  configuration?: string;
+  noBuild?: string;
+  baseHref?: string;
+  accessKeyId?: string;
+  secretAccessKey?: string;
+  region?: string;
+  bucket?: string;
+  subFolder?: string;
 }
--- libs/ngx-aws-deploy/src/lib/ng-add/schema.d.ts
+++ libs/ngx-aws-deploy/src/lib/ng-add/schema.d.ts
@@ -1,34 +1,6 @@
 export interface Schema {
-  project?: Project;
-  region?: Region;
-  bucket?: Bucket;
-  subFolder?: Bucket;
-}
-
-export interface Bucket {
-  type?: string;
-  description?: string;
-  'x-prompt'?: string;
-}
-
-export interface Project {
-  type?: string;
-  description?: string;
-}
-
-export interface Region {
-  type?: string;
-  description?: string;
-  'x-prompt'?: XPrompt;
-}
-
-export interface XPrompt {
-  message?: string;
-  type?: string;
-  items?: Item[];
-}
-
-export interface Item {
-  value?: string;
-  label?: string;
+  project?: string;
+  region?: string;
+  bucket?: string;
+  subFolder?: string;
 }
