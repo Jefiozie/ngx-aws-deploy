69822e9b97c7df7f03834ad709262ea83a5f7b5d
Jefiozie <jefiozie.bosch@gmail.com>
2021-05-30 14:15:05 +0200

    feat: add support for default aws variables closes: #372

--- libs/ngx-aws-deploy/src/lib/deploy/config.ts
+++ libs/ngx-aws-deploy/src/lib/deploy/config.ts
@@ -1,11 +1,11 @@
 import { Schema } from './schema';
 
 export const getAccessKeyId = (): string => {
-  return process.env.NG_DEPLOY_AWS_ACCESS_KEY_ID as string;
+  return process.env.NG_DEPLOY_AWS_ACCESS_KEY_ID as string || process.env.AWS_ACCESS_KEY_ID as string;
 };
 
 export const getSecretAccessKey = (): string => {
-  return process.env.NG_DEPLOY_AWS_SECRET_ACCESS_KEY as string;
+  return process.env.NG_DEPLOY_AWS_SECRET_ACCESS_KEY as string || process.env.AWS_SECRET_ACCESS_KEY as string;
 };
 
 export const getBucket = (builderConfig: Schema): string => {
@@ -13,7 +13,7 @@ export const getBucket = (builderConfig: Schema): string => {
 };
 
 export const getRegion = (builderConfig: Schema): string => {
-  return process.env.NG_DEPLOY_AWS_REGION || (builderConfig.region as string);
+  return process.env.NG_DEPLOY_AWS_REGION || (builderConfig.region as string) || process.env.AWS_DEFAULT_REGION;
 };
 
 export const getSubFolder = (builderConfig: Schema): string => {
